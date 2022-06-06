7f49a8978e652d31691afa6fcfb984a4cfc1cd96
Kirill <kir.badyuk94@gmail.com>
2022-06-06 22:22:16 +0400

    primer 1 gotov

---
+++ Lesson 1, learn language C# 06-06-2022/c sharp/Program.cs
@@ -0,0 +1,16 @@
+﻿int NumberA = 25;
+Console.WriteLine(NumberA);
+int NumberB = 6;
+Console.WriteLine(NumberB);
+
+if(NumberB * NumberB == NumberA)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.Write("NO");
+}
+
+
+
