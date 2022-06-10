80a19cf426237f278800af67741ab52e9d4c19b0
Kirill <kir.badyuk94@gmail.com>
2022-06-10 10:36:16 +0400

    finish 2 lection

---
+++ Lections. Learn Language C#, start 04.06.2022/Examples/Example10_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {91, 72, 83, 4, 4, 85, 36, 47, 18};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
