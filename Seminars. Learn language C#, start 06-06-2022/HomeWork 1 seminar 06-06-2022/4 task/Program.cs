b9ea4accc1eda26cc316ca46858f116a9d20654d
Kirill <kir.badyuk94@gmail.com>
2022-06-10 11:22:26 +0400

    add 4 task

---
+++ Seminars. Learn language C#, start 06-06-2022/HomeWork 1 seminar 06-06-2022/4 task/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите длинну: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+while (res < n)
+{
+    if (res == 1)
+    {
+        res += 1;
+    }
+    System.Console.WriteLine(res);
+    res += 2;
+}
+
+
