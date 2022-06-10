a6bb567621c5454527ed8515928024f7fb83c57e
Kirill <kir.badyuk94@gmail.com>
2022-06-10 10:04:46 +0400

    add 4 task

---
+++ Lesson2 C#/4 задача/Program.cs
@@ -0,0 +1,18 @@
+﻿int NumberA = 5;
+Console.WriteLine(NumberA);
+int NumberB = 25;
+Console.WriteLine(NumberB);
+
+int result = NumberA * NumberA;
+
+if(NumberA * NumberA == NumberB)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.Write("NO");
+}
+
+
+
