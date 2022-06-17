181590ca84a37b4b7dab786805dc684c07d77327
Kirill <kir.badyuk94@gmail.com>
2022-06-17 21:29:42 +0400

    modified 3 task

--- Seminars. Learn language C#, start 06-06-2022/Lesson 3. learn  C#, Methods/task3/Program.cs
+++ Seminars. Learn language C#, start 06-06-2022/Lesson 3. learn  C#, Methods/task3/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных
+// координат точек в этой четверти (x и y).
+
+void CoordinateDemo(int part)
+{
+    if (part < 1 || part > 4)
+    {
+        Console.WriteLine("Заданы не верные параметры");
+    }
+    else
+    {
+        if(part == 1) System.Console.WriteLine("x > 0 и y > 0");
+        if(part == 2) System.Console.WriteLine("x < 0 и y > 0");
+        if(part == 3) System.Console.WriteLine("x < 0 и y < 0");
+        if(part == 4) System.Console.WriteLine("x > 0 и y < 0");
+    }
+}
+CoordinateDemo(1);
+CoordinateDemo(4);
+CoordinateDemo(2);
+CoordinateDemo(3);
+CoordinateDemo(8);
