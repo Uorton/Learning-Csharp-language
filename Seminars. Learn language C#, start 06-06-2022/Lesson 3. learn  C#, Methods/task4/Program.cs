ed83a55152664bffe5bd69d85cf1c0cb4ed797bd
Kirill <kir.badyuk94@gmail.com>
2022-06-17 21:39:39 +0400

    add and modified task4

---
+++ Seminars. Learn language C#, start 06-06-2022/Lesson 3. learn  C#, Methods/task4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и 
+// находит расстояние между ними в 2D пространстве.
+// Вариант А - A(3,6); B(2,1) -> 5,09
+
+
+void Distance (int x, int y, int x1, int y1)
+{
+    int d = ((x1-x)*(x1-x)+(y1-y)*(y1-y));
+    double sqr = Math.Sqrt(d);
+    Console.WriteLine(sqr);
+}
+//Distance(3, 6, 2, 1);
+
+
+// Вариант Б - A(7, -5); B(1, -1) -> 7,21
+
+
+void Distance1 (int x, int y, int x1, int y1)
+{
+    int d = ((x1-x)*(x1-x)+(y1-y)*(y1-y));
+    double sqr = Math.Sqrt(d);
+    Console.WriteLine(sqr);
+}
+Distance1(7, -5, 1, -1);
