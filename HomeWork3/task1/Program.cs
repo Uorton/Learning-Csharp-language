cd85b28b098dc734a8407952abc58c255c109395
Kirill <kir.badyuk94@gmail.com>
2022-06-19 00:05:04 +0400

    add task1 and completed question

---
+++ HomeWork3/task1/Program.cs
@@ -0,0 +1,13 @@
+﻿string Palindrom = Convert.ToString(Console.ReadLine());
+char[] obrtext = Palindrom.ToCharArray();
+Array.Reverse(obrtext);
+string finaltext = new string(obrtext);
+
+if (Palindrom == finaltext)
+ {
+    Console.WriteLine("Данная запись является палиндромом");
+ }
+if (Palindrom != finaltext)
+ {
+    Console.WriteLine("Данная запись не является палиндромом");
+ }
