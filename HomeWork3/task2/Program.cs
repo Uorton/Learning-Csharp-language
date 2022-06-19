0509593639c2550294a990dd6013a8b80d088f54
Kirill <kir.badyuk94@gmail.com>
2022-06-19 21:37:09 +0400

    add task2 and completed question

---
+++ HomeWork3/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и 
+// находит расстояние между ними в 3D пространстве.
+// Вариант А - A(3,6,8); B(2,1, -7) -> 15,84
+
+
+void Distance (int x, int y, int z, int x1, int y1, int z1)
+{
+    int d = ((x1-x)*(x1-x)+(y1-y)*(y1-y)+(z1-z)*(z1-z));
+    double sqr = Math.Sqrt(d);
+    double sqr1 = Math.Round(sqr, 2);
+    Console.WriteLine(sqr1);
+}
+Distance(3, 6, 8, 2, 1, -7);
