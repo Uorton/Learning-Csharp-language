be91ebe0e92137a4f086a46f6960bca6dec5e2ca
Kirill <kir.badyuk94@gmail.com>
2022-06-21 06:52:54 +0400

    modified example012 and example014

--- Lections. Learn Language C#, start 04.06.2022/Examples/Example012_Methods/Program.cs
+++ Lections. Learn Language C#, start 04.06.2022/Examples/Example012_Methods/Program.cs
@@ -1,17 +1,17 @@
 ﻿// Вид 1
 void Method1()
 {
-    Console.WriteLine("Author Kirill Badyuk");
+    //Console.WriteLine("Author Kirill Badyuk");
 }
-//Method1();
+Method1();
 
 
 // Вид 2
-void Method2(string msg)
+void Method212(string msg)
 {
     Console.WriteLine(msg);
 }
-Method2(msg:"Text message");
+Method212(msg:"Text message");
 
 void Method21(string msg, int count)
 {
@@ -22,8 +22,8 @@ void Method21(string msg, int count)
         i++;
     }
 }
-//Method21(msg: "Text", count: 4);
-//Method21(count: 4, msg: "new message");
+Method21(msg: "Text", count: 4);
+Method21(count: 4, msg: "new message");
 
 
 //Вид 3 - Чтото возвращает, но ничего не принимает.
@@ -52,4 +52,112 @@ string Method4(int count, string text)
 }
 
 string res = Method4(10, "z");
-Console.WriteLine(res);
+//Console.WriteLine(res);
+
+
+
+// Вид 5 - метод For
+
+string Method5(int count, string text)
+{
+    
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string result = Method5(10, "z");
+//Console.WriteLine(res);
+
+
+
+// Вид 6 - методы и цикл в цикле.
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 0; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i*j}");
+    }
+    Console.WriteLine();
+
+}
+ // Значок $ - обозначает интерполяцию строк.
+
+
+//===Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+
+//Ясна ли задача?
+
+//string text = "-Я думаю, - сказал князь, улыбаясь, - что, "
+            //+ "ежели бы вас послали вместо нашего милого Винценгероде,"
+            //+ "вы бы взяли приступом согласие прусского короля. "
+//            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+//string newText = Replace(text, ' ', '|');
+//Console.WriteLine(newText);
+//Console.WriteLine();
+//newText = Replace(newText, 'к', 'К');
+//Console.WriteLine(newText);
+
+
+
+int [] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
---
+++ Lections. Learn Language C#, start 04.06.2022/Examples/example014/Program.cs
@@ -0,0 +1,31 @@
+﻿void SearchCoordinates(int X, int Y)
+{
+    if (X == 0 || Y == 0)
+    {
+        Console.WriteLine("Некорректные данные");
+    }
+    else
+    {
+        if(X > 0 && Y > 0)
+        {
+            System.Console.WriteLine("1");
+        }
+        if(X > 0 && Y <0)
+        {
+            System.Console.WriteLine("4");
+        }
+        if(X < 0 && Y > 0)
+        {
+            System.Console.WriteLine("2");
+        }
+        if(X < 0 && Y < 0)
+        {
+            System.Console.WriteLine("3");
+        }
+    }
+}
+SearchCoordinates(1, 3);
+SearchCoordinates(-1, 3);
+SearchCoordinates(1, -3);
+SearchCoordinates(-1, -3);
+SearchCoordinates(0, 0);
