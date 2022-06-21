668e5b2b60f5edf24dac808d53f6f10d79cbaa3d
Kirill <kir.badyuk94@gmail.com>
2022-06-21 06:52:08 +0400

    add task 1-4 and complete questions

---
+++ Seminars. Learn language C#, start 06-06-2022/Lesson 4, learn C#, Methods2, 16-06-2022/task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдает произведение чисел от 1 до N.
+
+int Composition (int N)
+{
+    int result = 1;
+    if (N > 0)
+    {
+    for (int i = 1; i <= N; i++)
+    {
+        result = result * i;
+    }
+    return result;
+    }
+    else
+    {
+        System.Console.WriteLine("0");
+    } 
+}
+
+Console.WriteLine(Composition(5));
+Console.WriteLine(Composition(4));
