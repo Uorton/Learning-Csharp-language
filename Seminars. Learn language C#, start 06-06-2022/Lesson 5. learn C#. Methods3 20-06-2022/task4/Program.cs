641af91de3d039325afb7f5a0c047cde1489474e
Kirill <kir.badyuk94@gmail.com>
2022-06-26 00:19:30 +0400

    add task4 and task4 variant 2

--- Seminars. Learn language C#, start 06-06-2022/Lesson 5. learn C#. Methods3 20-06-2022/task4/Program.cs
+++ Seminars. Learn language C#, start 06-06-2022/Lesson 5. learn C#. Methods3 20-06-2022/task4/Program.cs
@@ -2,26 +2,41 @@
 // Парой считаем первый и последний элемент, второй и предпоследний, и т.д.
 // Результат запишите в новом массиве.
 
-void CompositionOfPairds(int[] array)
+int[] CreateArray()
 {
-    int[] result = Convert.ToInt32(Console.ReadLine());
-
-    for (int i = 0; i < result.Length; i++)
+    int[] array = new int[11];
+    for (int i = 0; i < array.Length; i++)
     {
-        if (array.Length % 2 == 0)
-        {
-            result = new int[array.Length / 2];
-            result[i] = array[i] * array[array.length - i - 1];
-        }
-        else
-        {
-            result = new int[array.Length / 2 + 1];
-        }
-        
-        for (int i = 0; i < result.Length; i++)
-        {
-            result[i] = array[i] * array[array.Length / 2];
-        }
-        Console.WriteLine(result);
+        array[i] = new Random().Next(1, 10);
+        Console.Write($"{array[i]}, ");
+    }
+    return array;
+}
+int [] arr = CreateArray();
+
+int[] CreateSumArrayElements(int[] array)
+{
+    int[] number = new int [5];
+    int length = array.Length;
+
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        number[i] = array[i] * (array[length - 1]);
+        length = length - 1;
+    }
+    return number;
+}
+
+int[] number = CreateSumArrayElements(arr);
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
     }
 }
+
+Console.WriteLine();
+PrintArray(number);
+
