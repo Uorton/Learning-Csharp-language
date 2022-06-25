f04ab5c756c6ede98536f9dc464e3aab1f6e8e10
Kirill <kir.badyuk94@gmail.com>
2022-06-25 21:40:04 +0400

    add homework5, complete task1-3

---
+++ Homework5/task1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте массив заполненный случайными положительными
+// трехзначными числами. Напишите программу, 
+// которая покажет количество четных чисел в массиве.
+
+int[] CreateAndFillArray()
+{
+    int [] array = new int[5];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+        
+    }
+    return array;
+}
+
+int [] arr = CreateAndFillArray();
+
+void FindArray(int [] array)
+{
+    int number = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {    
+            Console.Write($"{array[i]}, ");
+            number++;
+        }
+        else
+        {
+          Console.Write($"{array[i]}, ");
+        }
+    }
+    Console.Write("\nКоличество четных чисел: ");
+    Console.Write(number);
+}
+
+FindArray(arr);
---
+++ Homework5/task2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечетных позициях.
+
+int[] CreateArray()
+{
+    int[] array = new int [5];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 20);
+
+    }
+    return array;
+}
+
+int [] arr = CreateArray();
+
+void FindNoEvenArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    Console.Write($"{array[i]}, ");
+
+    }
+    Console.WriteLine("Сумма элементов на нечетных позициях: ");
+    Console.Write(sum);
+}
+FindNoEvenArray(arr);
