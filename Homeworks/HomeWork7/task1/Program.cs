21e3f6090fe60c410512bcc39d57efea108d8130
Kirill <kir.badyuk94@gmail.com>
2022-07-01 19:12:19 +0400

    add task1 and task2

---
+++ Homeworks/HomeWork7/task1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте двумерный массив размером m*n, заполненный
+// случайными вещественными числами.
+
+
+double[,] CreateArray(int n, int m)
+{
+    return new double[n, m];
+}
+
+void FillArray(double [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            
+            array[i,j] = new Random().NextDouble();
+        }
+        
+    }
+}
+
+void PrdoubleArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+double[,] arr = CreateArray(3, 4);
+FillArray(arr);
+PrdoubleArray(arr);
+
+
+
+
+
+
+
---
+++ Homeworks/HomeWork7/task2/Program.cs
@@ -0,0 +1,65 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента
+// в двумерном массиве, и возвращает значение этого элемента,
+// или же указание, что такого элемента нет.
+
+int[,] CreateArray(int n, int m)
+{
+    return new int[n, m];
+}
+
+void FillArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            
+            array[i,j] = new Random().Next(5, 10);
+        }
+        
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void FindArray(int[,] array)
+{
+    int element;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            element = array[i,j];
+            if (element == i && element == j)
+            {
+                Console.WriteLine("Такого элемента нет");                
+            }
+            else
+            {
+                Console.WriteLine($"Позиция элемента в двумерном массиве: {element}");
+            }
+            break;
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] arr = CreateArray(3, 4);
+FillArray(arr);
+PrintArray(arr);
+FindArray(arr);
+Console.WriteLine();
+
+
+
