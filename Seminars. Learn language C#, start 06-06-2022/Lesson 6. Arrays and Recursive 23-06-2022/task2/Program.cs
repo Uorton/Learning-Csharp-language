7e850ced127583caffd440ec1641b28eba33a70c
Kirill <kir.badyuk94@gmail.com>
2022-07-01 15:06:25 +0400

    add lesson 6(and 2 tasks), lesson 7(and 4 tasks), and start 8 lesson

---
+++ Seminars. Learn language C#, start 06-06-2022/Lesson 6. Arrays and Recursive 23-06-2022/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и проверяет, может ли существовать треугольник со сторонами
+// такой длинны.
+
+Console.WriteLine("Проверим, может ли введенные вами числа быть сторонами треугольника");
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a < b + c && b < a + c && c < a + b)
+Console.WriteLine("Введенные вами цифры могут быть сторонами треугольника");
+else
+{
+    Console.WriteLine("Введенные вами цифры не могут быть сторонами треугольника");
+}
