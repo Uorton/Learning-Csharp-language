598d4ed7983a8414cb38469846bd1eab74ff5a5b
Kirill <kir.badyuk94@gmail.com>
2022-07-04 21:27:38 +0400

    Add gitignore, 1 lesson, task 1 and task 2

---
+++ Geekbrains.Семинары C# старт 06-06-2022/Lesson 1. C# 06-06-2022/task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+string number = "Console.ReadLine()";
+
+if(number == "3")
+{
+    Console.WriteLine("Среда");
+}
+if(number == "5")
+{
+    Console.WriteLine("Пятница");
+}
